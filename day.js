641bedaf2bbc6415fdd3a3d0dbb5d2b718034d56
evets122coding <evets122coding@gmail.com>
2021-04-27 17:24:34 -0400

    Functional map and filter

--- day.js
+++ day.js
@@ -1,3 +1,4 @@
+// Returns the day of the week for the given date.
 function dayName(date) {
   const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
                          "Thursday", "Friday", "Saturday"];
