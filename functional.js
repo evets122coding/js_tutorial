83307f5f87e0fca3b7f1ae0004420d52df4b7974
evets122coding <evets122coding@gmail.com>
2021-04-28 16:28:05 -0400

    Add reduce function

--- functional.js
+++ functional.js
@@ -39,3 +39,42 @@ function functionalSingles(elements) {
   return elements.filter(element => element.split(/\s+/).length === 1);
 }
 console.log(functionalSingles(states));
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function(n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+  return numbers.reduce((total, n) => { return total += n; });
+}
+console.log(functionalSum(numbers));
+
+
+// lengths: Imperative solution
+function imperativeLentghs(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  })
+  return lengths;
+}
+console.log(imperativeLentghs(states));
+
+
+// lengths: Functional solution
+function functionalLentghs(elements) {
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+console.log(functionalLentghs(states));
